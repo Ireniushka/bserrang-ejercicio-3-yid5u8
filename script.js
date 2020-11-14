a3609aae8bfcadd2b33d602f04f928a51e8ab762
Ireniushka <irenerengelruiz@gmail.com>
2020-11-14 18:55:30 +0100

    ejercicio realizado

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
+// En el siguiente ejercicio debéis sacar por consola (console.log) una lista con el nombre de los usuarios.
 
+// Dicha la lista la obtendréis del documento "users.json" haciendo uso de la función "fetch".  
+
+//Una vez tengáis el array de usuarios, debéis generar otro array con la función "filter" para que solo estén los usuarios que tengán más de 500 en la propiedad "money".
+
+// Teniendo la lista ya filtrada, la recorreréis con el método "forEach" para mostrar por consola el nombre de cada usuario.
 
 let buttonShowList = document.getElementById("showList");
 buttonShowList.addEventListener("click", getUsers);
 
 function getUsers() {
-  // En el siguiente ejercicio debéis sacar por consola (console.log) una lista con el nombre de los usuarios.
-  // Dicha la lista la obtendréis del documento "users.json" haciendo uso de la función "fetch". 
-
-  let users = fetch('./users.json').then(response => response.json()).then(data => console.log(data));
-
-  //Una vez tengáis el array de usuarios, debéis generar otro array con la función "filter" para que solo estén los usuarios que tengán más de 500 en la propiedad "money".
-
-  let usersFilter = () => users.filter(user => user.money > 500);
-
-  console.log(usersFilter);
-
-  // Teniendo la lista ya filtrada, la recorreréis con el método "forEach" para mostrar por consola el nombre de cada usuario.
-
-  function print(){
-    usersFilter.forEach(user => console.log(user))
-  }
-
-  console.log(print);
-
+  fetch('./users.json').then(response => response.json()).then(data => data.filter(user => user.money > 500).forEach(user => console.log(user.name)));
 }
